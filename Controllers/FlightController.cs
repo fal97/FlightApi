3594e45cb0b8bd3d9f484f649e33a9de7b1fe0cb
Abijan Udara <abijan@bistecglobal.com>
2024-02-15 14:24:44 +0530

    Add project files.

---
+++ Controllers/FlightController.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Web.Http;
+using HttpGetAttribute = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
+using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
+
+namespace FlightApi.Controllers
+{
+    public class FlightController : ApiController
+    {
+        private IFlightService _IFileService;
+
+        public FlightController(IFlightService service)
+        {
+            _IFileService = service;
+        }
+
+        [HttpGet]
+        public List<FlightEntity> GetFlightEntities()
+        {
+            List<FlightEntity> flights = new List<FlightEntity>();
+
+            flights = _IFileService.GetAll();
+
+            return flights;
+
+        }
+
+
+
+        [HttpPost]
+        public void PostFlightEntities(FlightEntity flight)
+        {
+            _IFileService.AddFlightEntity(flight);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+using Application;
+using Domain.Interfaces;
+using Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IFlightService,FlightService>();
+builder.Services.AddScoped<IFlightDbContext,FlightDbContext>();
+
+//builder.Services.AddDbContext<FlightDbContext>(options =>
+//                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
